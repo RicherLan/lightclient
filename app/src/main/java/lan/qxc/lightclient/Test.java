b9fac1910299076a4923acaff739aee1702b7317
qiuxiaochi <1348270542@qq.com>
2020-04-23 15:47:19 +0800

    删除无用代码

--- app/src/main/java/lan/qxc/lightclient/Test.java
+++ app/src/main/java/lan/qxc/lightclient/Test.java
@@ -1,43 +1,9 @@
 package lan.qxc.lightclient;
 
-import android.content.Intent;
-import android.widget.Toast;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-
-import lan.qxc.lightclient.entity.Dongtai;
-import lan.qxc.lightclient.entity.DongtailVO;
-import lan.qxc.lightclient.entity.User;
-import lan.qxc.lightclient.entity.UserVO;
-import lan.qxc.lightclient.https.BaseCallBack;
-import lan.qxc.lightclient.https.BaseOkHttpClient;
 import lan.qxc.lightclient.result.Result;
-import lan.qxc.lightclient.retrofit_util.RetrofitHelper;
-import lan.qxc.lightclient.retrofit_util.api.UserAPI;
-import lan.qxc.lightclient.retrofit_util.service.DongtaiRequestService;
-import lan.qxc.lightclient.retrofit_util.service.UserRequestService;
-import lan.qxc.lightclient.service.DongtaiServicce;
+
 import lan.qxc.lightclient.service.GuanzhuService;
-import lan.qxc.lightclient.service.UserService;
-import lan.qxc.lightclient.ui.activity.home.HomeActivity;
-import lan.qxc.lightclient.ui.activity.user_activitys.LoginActivity;
-import lan.qxc.lightclient.util.JsonUtils;
-import lan.qxc.lightclient.util.MyDialog;
-import okhttp3.RequestBody;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
